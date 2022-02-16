c63eab96147caa88199b61a7012aa988b5d1e9a6
Lalith Sharma <lalithsharma989@gmail.com>
2022-02-16 19:29:39 +0530

    First Commit

---
+++ app/src/main/java/com/lalithsharma/freshnews/News.kt
@@ -0,0 +1,16 @@
+package com.lalithsharma.freshnews
+
+   /* data class News(function: () -> String) {
+        val title: String,
+        val author:String,
+        val url: String,
+        val imageUri:String
+
+    }*/
+
+data class News(
+    val title: String,
+    val author: String,
+    val url: String,
+    val imageUrl:String
+)
